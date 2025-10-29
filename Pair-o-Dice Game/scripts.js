8f95b2511a494e9e941b31844ba7ce9ee7bdf4ec
Scottish.Red <scottish.red.heather@gmail.com>
2025-10-29 14:09:24 -0700

    Added a button to roll the dice and to look nicer.

--- Pair-o-Dice Game/scripts.js
+++ Pair-o-Dice Game/scripts.js
@@ -1,13 +1,15 @@
-var outputOne = "./images/dice" + [Math.floor(Math.random() * 6) + 1] + ".png";
-document.querySelector(".img1").setAttribute("src", outputOne);
+function btnRoll() {
+    var outputOne = "./images/dice" + [Math.floor(Math.random() * 6) + 1] + ".png";
+    document.querySelector(".img1").setAttribute("src", outputOne);
 
-var outputTwo = "./images/dice" + [Math.floor(Math.random() * 6) + 1] + ".png";
-document.querySelector(".img2").setAttribute("src", outputTwo);
+    var outputTwo = "./images/dice" + [Math.floor(Math.random() * 6) + 1] + ".png";
+    document.querySelector(".img2").setAttribute("src", outputTwo);
 
-if (outputOne > outputTwo) {
-    document.querySelector("h1").innerHTML = "Player 1 Wins!";
-    } else if (outputTwo > outputOne) {
-        document.querySelector("h1").innerHTML = "Player 2 Wins!";
-    } else if (outputOne === outputTwo) {
-        document.querySelector("h1").innerHTML = "It's a Draw!";
-    };
+    if (outputOne > outputTwo) {
+        document.querySelector("h1").innerHTML = "Player 1 Wins!";
+        } else if (outputTwo > outputOne) {
+            document.querySelector("h1").innerHTML = "Player 2 Wins!";
+        } else if (outputOne === outputTwo) {
+            document.querySelector("h1").innerHTML = "It's a Draw!";
+        };
+}
