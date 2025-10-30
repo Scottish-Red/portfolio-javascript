82be453829c43f39268a4eb15bfc736e071335fc
Scottish.Red <scottish.red.heather@gmail.com>
2025-10-30 10:44:46 -0700

    fixed a typo - needed caps in index.html

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -44,19 +44,19 @@ var buttonCount = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < buttonCount; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-        if (this.innerHTML === "w") {
+        if (this.innerHTML === "W") {
             new Audio("sounds/tom-1.mp3").play(); // Creates a new audio object & plays the sound on click
-        } else if (this.innerHTML === "a") {
+        } else if (this.innerHTML === "A") {
             new Audio("sounds/tom-2.mp3").play();
-        } else if (this.innerHTML === "s") {
+        } else if (this.innerHTML === "S") {
             new Audio("sounds/tom-3.mp3").play();
-        } else if (this.innerHTML === "d") {
+        } else if (this.innerHTML === "D") {
             new Audio("sounds/tom-4.mp3").play();
-        } else if (this.innerHTML === "j") {
+        } else if (this.innerHTML === "J") {
             new Audio("sounds/snare.mp3").play();
-        } else if (this.innerHTML === "k") {
+        } else if (this.innerHTML === "K") {
             new Audio("sounds/crash.mp3").play();
-        } else if (this.innerHTML === "l") {
+        } else if (this.innerHTML === "L") {
             new Audio("sounds/kick-bass.mp3").play();
         }
     });
